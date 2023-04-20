e02eb4614dc2f33de8681b82a69f4626f0113830
C4ncino <193305@iberopuebla.mx>
2023-04-19 18:30:35 -0600

    Adición de una tabla y uso en html

--- products/models.py
+++ products/models.py
@@ -5,7 +5,7 @@ class Product(models.Model):
     name = models.CharField(max_length=50)
     price = models.DecimalField(max_digits=7, decimal_places=2)
     stock = models.IntegerField()
-    description = models.CharField(max_length=250)
+    description = models.CharField(max_length=250, null=True, blank=True)
 
     def __str__(self) :
         return self.name
--- products/views.py
+++ products/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Product
 
 # Create your views here.
 class Products (View):
     def get(self, request):
-        products = [] #TODO: Hacer la querry
+        products = Product.objects.all()
         context = {'products': products}
         return render(request, 'products.html', context)
