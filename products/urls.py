d5f28533c7fb1773accdcada9e104a8e5627e758
C4ncino <193305@iberopuebla.mx>
2023-04-30 19:36:07 -0600

    Style and Brand COnfig

--- products/urls.py
+++ products/urls.py
@@ -6,7 +6,11 @@ urlpatterns = [
     path('', Products.as_view(), name='product'),
     path('create', CreateProduct.as_view(), name='create_product'),
     path('update/<int:pk>', UpdateProduct.as_view(), name='update_product'),
+    path('delete/<int:pk>', DeleteProduct.as_view(), name='delete_product'),
 
     
     path('brands', Brands.as_view(), name='brands'),
+    path('brand/create', CreateBrand.as_view(), name='create_brand'),
+    path('brand/update/<int:pk>', UpdateBrand.as_view(), name='update_brand'),
+    path('brand/delete/<int:pk>', DeleteBrand.as_view(), name='delete_brand'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views import View
-from products.forms import ProductCreate
+from products.forms import ProductCreate, BrandCreate
 from products.models import Product, Brand
 
 # Create your views here.
@@ -24,11 +24,10 @@ class CreateProduct(View):
         else:
             print("FORM ERROR!")
             print(form.errors)
-            return render(request, 'products/products.html', {'form': form})
+            return render(request, 'products/form.html', {'form': form})
 
 class UpdateProduct(View):  
     def get(self, request, pk):
-        
         product = Product.objects.get(id=pk)
         form = ProductCreate(instance = product)
 
@@ -36,18 +35,24 @@ class UpdateProduct(View):
         return render(request, 'products/form.html', context)
           
     def post(self, request, pk):
-
-        product = get_object_or_404(Product, id=pk)
-
-        form = ProductCreate(initial={'name': product.name})
+        product = Product.objects.get(id=pk)
+        form = ProductCreate(request.POST, instance=product)
 
         if form.is_valid():
             form.save()
             return redirect('product')
         else:
             print("FORM ERROR!")
-            print(form.errors)
-            return render(request, 'products/products.html', {'form': form})
+            print(form.errors.as_data())
+            return render(request, 'products/form.html', {'form': form})
+    
+
+class DeleteProduct(View):  
+    def get(self, request, pk):
+        product = Product.objects.get(id=pk)
+        product.delete()
+        
+        return redirect('product')
         
 
 class Brands(View):
@@ -56,6 +61,46 @@ class Brands(View):
         context = {'brands' : brands}
         return render(request, 'products/brands.html', context)
 
+class CreateBrand(View):
+    def get(self, request):
+        form = BrandCreate()
+        context = {'form': form}
+        return render(request, 'products/form_b.html', context)
+    
+    def post(self, request):
+        form = BrandCreate(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('brands')
+        else:
+            print("FORM ERROR!")
+            print(form.errors)
+            return render(request, 'products/form_b.html', {'form': form})
 
-# class CreateBrand(View):
-#     def get(self, request):
+class UpdateBrand(View):  
+    def get(self, request, pk):
+        brand = Brand.objects.get(id=pk)
+        form = BrandCreate(instance = brand)
+
+        context = {'form': form}
+        return render(request, 'products/form_b.html', context)
+          
+    def post(self, request, pk):
+        brand = Brand.objects.get(id=pk)
+        form = BrandCreate(request.POST, instance=brand)
+
+        if form.is_valid():
+            form.save()
+            return redirect('product')
+        else:
+            print("FORM ERROR!")
+            print(form.errors.as_data())
+            return render(request, 'products/form_b.html', {'form': form})
+    
+
+class DeleteBrand(View):  
+    def get(self, request, pk):
+        brand = Brand.objects.get(id=pk)
+        brand.delete()
+        
+        return redirect('brands')
