6171393ef1c78a253ff8ac1c1a81ad752bbcf0a5
JuanPabloGHC <jpghc552@hotmail.com>
2023-05-10 20:45:06 -0600

    logout y register

--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+
 # Create your views here.
 def login_user(request):
     if request.method == "POST":
@@ -11,8 +13,30 @@ def login_user(request):
             login(request, user)
             return redirect('home')
         else:
-            messages.success(request, ('There was an error loggin, try again...'))
+            messages.success(request, ("There was an error loggin, try again..."))
             return redirect('login')
 
     else:
         return render(request, 'authenticate/login.html', {})
+    
+def logout_user(request):
+    logout(request)
+    return redirect('home')
+
+def register_user(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("Registration Successful!"))
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'authenticate/register_user.html', {
+        'form':form,
+        })
