6be8e769e69585a187c345b071e4b96f7294dbce
C4ncino <193305@iberopuebla.mx>
2023-05-15 17:22:22 -0600

    Settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bjk*66$9l20wc%8x^0gic!+)=!bjbus%a(%!@(c+szk-8!pw!a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
