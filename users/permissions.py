48ed3284420b5a4e274d974fda2854a481d679af
C4ncino <193305@iberopuebla.mx>
2023-05-14 23:48:37 -0600

    Permissions

--- users/permissions.py
+++ users/permissions.py
@@ -1,21 +1,37 @@
 STAFF = [
-    'products.add_product',
-    'products.change_product',
-    'products.delete_product',
-    'products.view_product',
-    'products.add_brand',
-    'products.change_brand',
+    'products.add_product', 
+    'products.change_product'
+    'products.view_product', 
+    'products.delete_product', 
+
+    'products.add_brand', 
+    'products.change_brand', 
+    'products.view_brand', 
     'products.delete_brand',
-    'products.view_brand',
-    'shopping_cart.add_cart',
-    'shopping_cart.change_cart',
-    'shopping_cart.delete_cart',
-    'shopping_cart.view_cart'
+
+    'auth.add_user', 
+    'auth.change_user', 
+    'auth.view_user',
+    'auth.delete_user', 
+
+    'auth.view_group'
 ]
 
 CLIENT = [
-    'products.view_product',
-    'products.view_brand',
-    'shopping_cart.change_cart',
-    'shopping_cart.view_cart'
+    'products.change_product'
+    'products.view_product', 
+
+    'products.view_brand', 
+
+    'shopping.add_cart', 
+    'shopping.change_cart', 
+    'shopping.view_cart',
+
+    'shopping.add_order', 
+    'shopping.change_order',
+    'shopping.view_order',
+    'shopping.delete_order', 
+
+    'auth.change_user', 
+    'auth.view_user',
 ]
